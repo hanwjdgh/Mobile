cc3e3435310bb649a2111f54db55d33029a0af91
hanwjdgh <hanwjdgh@naver.com>
2017-03-22 21:06:24 +0900

    modify

---
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/SplachActivity.java
@@ -0,0 +1,24 @@
+package org.androidtown.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by GE62 on 2017-03-22.
+ */
+
+public class SplachActivity extends Activity{
+   @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+       try{
+           Thread.sleep(10000);
+       }
+       catch (InterruptedException e){
+           e.printStackTrace();
+       }
+       startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
